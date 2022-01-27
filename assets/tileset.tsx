68612bb326c1cb78236d24f880dcf6b215099707
lxxxxl <ne.lxx@ya.ru>
2022-01-27 21:31:35 +0300

    res: update tileset

--- assets/tileset.tsx
+++ assets/tileset.tsx
@@ -1,4 +1,179 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="tileset" tilewidth="16" tileheight="16" tilecount="77" columns="7">
  <image source="tileset.png" width="112" height="176"/>
+ <tile id="0">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="42">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="43">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="44">
+  <properties>
+   <property name="type" type="int" value="2"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="type" type="int" value="1"/>
+  </properties>
+ </tile>
+ <tile id="54">
+  <properties>
+   <property name="type" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="type" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="type" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="type" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="type" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="type" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="63">
+  <properties>
+   <property name="type" type="int" value="3"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="type" type="int" value="3"/>
+  </properties>
+ </tile>
 </tileset>
